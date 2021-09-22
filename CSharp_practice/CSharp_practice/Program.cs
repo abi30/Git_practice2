51447cf60ee64008968decc8d4f2a4d0f3dc3dca
abi30 <rakib0751@gmail.com>
2021-09-22 22:57:14 +0200

    all done

---
+++ CSharp_practice/CSharp_practice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Rakib");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Git_practice2/Git_practice2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("practice with while loop");
+
+            int number = 1;
+
+            while(number<30)
+            {
+                number++;
+                if (number%2==0)
+                {
+                    continue;
+                  
+                    //break;
+                }
+                Console.WriteLine($"hello bangladesh {number}");
+                
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
