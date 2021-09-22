47b44109966e4800332e08c4233dbde56ad34878
abi30 <rakib0751@gmail.com>
2021-09-23 01:45:19 +0200

    string, array, enum

--- CSharp_practice/CSharp_practice/Program.cs
+++ CSharp_practice/CSharp_practice/Program.cs
@@ -10,19 +10,29 @@ namespace CSharp_practice
     class Program
     {
         static void Main(string[] args)
-        {
-
-
+        {  // Class 
             Person p1 = new Person();
 
             p1.firstName = "Abdulla";
             p1.lastName = "Rakib";
-
             p1.Introduce();
-
-            int ans = Calculator.Add(3,4);
+            //ststic method 
+            int ans = Calculator.Add(3, 4);
             Console.WriteLine($"you have got in math {ans}");
 
+            // array in C#
+            string[] cars = { "BMW", "Volkwagen", "Ford", "Land rober", "mercedes" };
+            cars[3] = "toyota";
+            Console.WriteLine(cars[3]);
+            // String in Csharp
+
+            string fName = "abdullah ";
+            string lName = "mahabub";
+
+            string fullName = string.Concat(fName,lName);
+            Console.WriteLine(fullName);
+            Console.WriteLine(fullName.IndexOf('u'));
+            Console.WriteLine(fullName[3]);
 
             Console.ReadKey();
         }
